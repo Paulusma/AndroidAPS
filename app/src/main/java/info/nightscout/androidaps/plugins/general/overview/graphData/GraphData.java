1e5d57f2ea8d928703636ac4e45a9aab6557dcc4
Paulus <celestia63@gmail.com>
2019-07-28 12:56:07 +0200

    ...

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/graphData/GraphData.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/graphData/GraphData.java
@@ -420,7 +420,7 @@ public class GraphData {
         actSeriesHist = new FixedLineGraphSeries<>(actData);
         actSeriesHist.setDrawBackground(false);
         actSeriesHist.setColor(MainApp.gc(R.color.activity));
-        actSeriesHist.setThickness(3);
+        actSeriesHist.setThickness(2);
 
         addSeries(actSeriesHist);
 
@@ -430,7 +430,7 @@ public class GraphData {
 
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(3);
+        paint.setStrokeWidth(2);
         paint.setPathEffect(new DashPathEffect(new float[]{4, 4}, 0));
         paint.setColor(MainApp.gc(R.color.activity));
         actSeriesPred.setCustomPaint(paint);
